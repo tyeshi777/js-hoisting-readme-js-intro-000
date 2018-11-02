505b76e14b83f56a4625bf87c5c5c32484af0ef9
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-02 00:10:45 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,11 +1,10 @@
 function callMe() {
   var lyric = "maybe";
+  return lyric;
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
-  
 }
 
 
